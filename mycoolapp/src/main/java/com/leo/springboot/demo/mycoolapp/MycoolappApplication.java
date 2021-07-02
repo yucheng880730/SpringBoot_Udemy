83ec4e49bf3598133d263389bdac6ee8049c91cb
yucheng <yucheng2k13@gmail.com>
2021-07-02 11:16:30 +0800

    first commit

---
+++ mycoolapp/src/main/java/com/leo/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -0,0 +1,15 @@
+package com.leo.springboot.demo.mycoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycoolappApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MycoolappApplication.class, args);
+
+	}
+
+}
